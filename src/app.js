c287dda7191fa49c0c4d18f0adfd65ae235d5ed3
Poh <minmarnoo@gmail.com>
2020-03-17 15:23:35 +0800

    profile

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const express = require ('express')
 const app = express()
+const hbs = require('hbs')
+const path = require ('path')
+
 
 const port = process.env.PORT | 3000
 
+// PATHS
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
+
+// app
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+
 app.get('', (req, res) => {
-    res.send('Hello friend')
+    res.render('index')
 })
 
-app.listen(port, (port) => {
+app.listen(port, () => {
     console.log(`Portfolio site is running on port: ${port}`)
 })
